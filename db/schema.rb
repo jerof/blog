e71dae9a6b1d8957f7da710fb3c5b27f7c7c86a8
Nikhil <nikhilsamuel.89@gmail.com>
2018-09-21 14:53:16 -0400

    Create users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,21 @@
 
 ActiveRecord::Schema.define(version: 2018_09_21_183108) do
 
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
