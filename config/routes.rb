3ee1acbb4ee60b47ee934875e78af50960862383
Nikhil <nikhilsamuel.89@gmail.com>
2018-09-21 14:36:20 -0400

    Add users and comments scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :users
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
